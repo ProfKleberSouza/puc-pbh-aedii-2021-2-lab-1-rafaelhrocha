dd31d9579a80e28d4957affba6c786b98f16714a
Rafael <>
2021-08-20 01:19:49 +0000

    teste2

--- exe02/main.c
+++ exe02/main.c
@@ -3,19 +3,19 @@
 
 int main() {
 
-    int a, r;
+  int a, r;
   
   // ENTRADA DE DADOS
   scanf("%d", &a);
 
-  b = [a];
+  int b[a];
 
   for( int i = 0; i < a; i++){
-    scanf("%d", &b[i])
+    scanf("%d", &b[i]);
   }
   
   // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  r = somar(a, b);
+  r = menor(b, a);
 
   // SAIDA - IMPRIMINDO O RESULTADO
   printf("MENOR = %d\n", r);
